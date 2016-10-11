767e4f7a9abec0cfe716b2044934ec11de0281c1
Alex Cortelyou <acortelyou@gmail.com>
2016-10-11 16:31:09 -0700

    v.8.3

--- TierPlus.user.js
+++ TierPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @namespace    https://github.com/acortelyou/userscripts
 // @name         TierPlus
-// @version      0.8.2
+// @version      0.8.3
 // @author       Alex Cortelyou
 // @description  Tier injector for Yahoo FF
 // @thanks       to Boris Chen for publishing his FF tier data
